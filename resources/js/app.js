2d012f0abe6922f23a0f0724f8a7f28e7fa43183
Ebn Sina <ebnsina.me@gmail.com>
2020-12-21 16:45:19 +0600

    vue warn remove

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ window.Vue = require('vue');
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-const app = new Vue({
-    el: '#app',
+window.addEventListener('load', function () {
+    const app = new Vue({
+        el: '#app',
+    })
 })
