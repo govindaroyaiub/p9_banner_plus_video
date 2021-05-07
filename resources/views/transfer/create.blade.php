ba5e770438b64b56915ffee8a69471bde8a3d4bb
Ebn Sina <ebnsina.me@gmail.com>
2021-05-07 12:43:27 +0600

    drag and drop button remove

--- resources/views/transfer/create.blade.php
+++ resources/views/transfer/create.blade.php
@@ -52,7 +52,7 @@
 
                 <div class="flex flex-col flex-grow mb-3">
                     <div x-data="{ files: null }" id="FileUpload"
-                        class="block w-full py-2 px-3 relative bg-white appearance-none border-2 border-gray-300 border-dashed rounded-md hover:shadow-outline-gray rounded-lg">
+                        class="block w-full py-10 px-3 relative bg-white appearance-none border-2 border-gray-300 border-dashed rounded-md hover:shadow-outline-gray rounded-lg">
                         <input type="file" multiple name="upload[]"
                             class="absolute inset-0 z-50 m-0 p-0 w-full h-full outline-none opacity-0"
                             x-on:change="files = $event.target.files; console.log($event.target.files);"
@@ -76,13 +76,9 @@
                         </template>
                         <template x-if="files === null">
                             <div class="flex flex-col space-y-2 items-center justify-center">
-                                <i class="fas fa-cloud-upload-alt fa-3x text-currentColor"></i>
-                                <p class="text-primary font-semibold">
+                                <p class="text-2xl font-semibold">
                                     Drag your ZIP files here or click in this area.
                                 </p>
-                                <a href="javascript:void(0)"
-                                    class="flex items-center mx-auto py-2 px-4 text-white text-center font-medium border border-transparent rounded-lg outline-none bg-blue-400">Select
-                                    a file</a>
                             </div>
                         </template>
                     </div>
