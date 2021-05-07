bb1a04082f050b1d0d7394046236e4f7dcc5ea11
Govinda Roy <govindaroy.ofc94@gmail.com>
2021-05-07 13:59:38 +0600

    Update

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/clear-cache', function() {
     $exitCode = Artisan::call('cache:clear');
     $exitCode = Artisan::call('view:clear');
     $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('route:clear');
     $exitCode = Artisan::call('config:cache');
 
     return 'Cache, View, Config cleared!';
