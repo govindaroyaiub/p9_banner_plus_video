55462cd695c168931c3d6f6d1cfa46c230808f22
Govinda Roy <govindaroy.ofc94@gmail.com>
2022-07-10 23:42:35 +0600

    Update

--- resources/views/view_banner/banner-develop.blade.php
+++ resources/views/view_banner/banner-develop.blade.php
@@ -268,7 +268,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                             </a>
-                            <a href="/banner/edit/version/{{$main_project_id}}/{{$id}}" class="text-blue-600">
+                            <a href="/banner/edit/version/{{$main_project_id}}/{{$id}}" class="text-blue-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                   </svg>
