974678b404fb18bb3e11f14a1eeafda28097ab70
Govinda Roy <govindaroy.ofc94@gmail.com>
2022-07-29 18:55:30 +0600

    Update

--- app/Http/Controllers/bannerShowcaseController.php
+++ app/Http/Controllers/bannerShowcaseController.php
@@ -25,4 +25,64 @@ class bannerShowcaseController extends Controller
     public function bannerShowcaseList(){
         return view('view_bannershowcase.showcase-list');
     }
+
+    public function banner_project_add_view(){
+
+    }
+
+    public function banner_project_add_post(){
+
+    }
+
+    public function banner_project_edit_view(){
+
+    }
+
+    public function banner_project_edit_post(){
+
+    }
+
+    public function banner_project_delete(){
+
+    }
+
+    public function banner_add_feedback_view(){
+
+    }
+
+    public function banner_add_feedback_post(){
+
+    }
+
+    public function banner_edit_feedback_view(){
+
+    }
+
+    public function banner_edit_feedback_post(){
+
+    }
+
+    public function banner_delete_feedback(){
+
+    }
+
+    public function banner_add_category_view(){
+
+    }
+
+    public function banner_add_category_post(){
+
+    }
+
+    public function banner_edit_category_view(){
+
+    }
+
+    public function banner_edit_category_post(){
+
+    }
+
+    public function banner_delete_category(){
+
+    }
 }
