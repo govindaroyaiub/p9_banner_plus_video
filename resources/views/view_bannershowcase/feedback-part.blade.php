cc8759dd8012b488980955e58586d1d78b4db114
Govinda Roy <govindaroy.ofc94@gmail.com>
2022-08-02 02:04:46 +0600

    update

--- resources/views/view_bannershowcase/feedback-part.blade.php
+++ resources/views/view_bannershowcase/feedback-part.blade.php
@@ -15,17 +15,17 @@
             <div x-show="show" class="border border-b-0 px-1 py-1 collapse" id="collapse{{$id}}" style="border-color: {{ $main_project_info['color'] }}">
                 @if(Auth::user())
                 <div class="flex float-right" style="z-index: 999;">
-                    <a href="/banner/add/version/{{$main_project_id}}/{{$id}}" class="text-green-600">
+                    <a href="/banner/add/feedback/{{$main_project_id}}/{{$id}}" class="text-green-600">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                     </a>
-                    <a href="/banner/edit/version/{{$main_project_id}}/{{$id}}" class="text-blue-500">
+                    <a href="/banner/edit/feedback/{{$main_project_id}}/{{$id}}" class="text-blue-500">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                           </svg>
                     </a>
-                    <a href="/delete/version/{{$main_project_id}}/{{$id}}" class="text-red-600" onclick="return confirm('Slow down HOTSHOT! You sure you want to delete this version?!');">
+                    <a href="/delete/feedback/{{$main_project_id}}/{{$id}}" class="text-red-600" onclick="return confirm('Slow down HOTSHOT! You sure you want to delete this version?!');">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                       </svg>
