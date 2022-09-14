68c95dbfe36e496f87ca83bc271ecd8e060bf1cd
Govinda Roy <govindaroy.ofc94@gmail.com>
2022-09-14 13:54:05 +0600

    Update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://creative.planetnine.com'),
+    'url' => env('APP_URL', 'http://localhost:8000'),
 
     'asset_url' => env('ASSET_URL', null),
 
