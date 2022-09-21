c56b99bd5214c16a6efbe9014d3deff357a41935
Govinda Roy <govindaroy.ofc94@gmail.com>
2022-09-21 12:48:06 +0600

    Update

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,20 +7,13 @@
                 class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/banner-showcase">Banner Showcase</a>
         </li>
         <li class="{{ request()->is('video-showcase') || request()->is('project/video-showcase/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/video-showcase">Video Showcase</a>
+                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/video-showcase">Video Showcase (on development)</a>
         </li>
         <li class="{{ request()->is('gif-showcase') || request()->is('project/gif-showcase/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
                 class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/gif-showcase">GIF Showcase (on development)</a>
         </li>
         <li class="{{ request()->is('social-showcase') || request()->is('project/social-showcase/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
                 class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/social-showcase">Social Image Showcase (on development)</a>
-        </li>
-
-        <li class="{{ request()->is('banner_sizes') || request()->is('banner_sizes/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/banner_sizes">Banner & GIF Sizes</a>
-        </li>
-        <li class="{{ request()->is('sizes') || request()->is('sizes/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/sizes">Video Sizes</a>
         </li>
 
         @if(Auth::user()->company_id == 1)
@@ -40,18 +33,18 @@
                 class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/p9_transfer">Files Transfer</a>
         </li>
         @endif
-        
+
         <li class="{{ request()->is('banner') || request()->is('project/banner/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/banner">Banner (Old System)</a>
+                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/banner">Banner (Old)</a>
         </li>
         <li class="{{ request()->is('video') || request()->is('project/video/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/video">Video (Old System)</a>
+                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/video">Video (Old)</a>
         </li>
         <li class="{{ request()->is('gif') || request()->is('project/gif/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/gif">GIF (Old System)</a>
+                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/gif">GIF (Old)</a>
         </li>
         <li class="{{ request()->is('social') || request()->is('project/social/*') ? 'bg-primary text-white rounded-lg' : '' }}"><a
-                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/social">Social Image (Old System)</a>
+                class="rounded-lg hover:bg-primary hover:text-white px-3 py-2 block" href="/social">Social Image (Old)</a>
         </li>
     </ul>
     @if(Auth::check())
