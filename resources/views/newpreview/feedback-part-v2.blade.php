8662180b3e6b2bef2b45d1218173e844f9affb0a
Govinda Roy <43463520+govindaroyaiub@users.noreply.github.com>
2023-11-09 17:51:46 +0600

    Update

--- resources/views/newpreview/feedback-part-v2.blade.php
+++ resources/views/newpreview/feedback-part-v2.blade.php
@@ -27,8 +27,8 @@
                 <i class="fa-regular fa-message"></i>
             </div>
             <div id="feedbackDescription">
-                <div id="feedbackDescriptionUpperpart" onclick="hideFeedbackMessage()">
-                    <div class="cursor-pointer" style="float: right;">
+                <div id="feedbackDescriptionUpperpart">
+                    <div class="cursor-pointer" style="float: right;" onclick="hideFeedbackMessage()">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>  
@@ -52,16 +52,34 @@
 
 <script>
     const userCompanyID = {{ $uploadedByCompany }};
+    var viewFeedback;
 
     $(document).ready(function(){
         getAllFeedbacks();
     });
 
     function showFeedbackMessage(){
+        viewFeedback = true;
+
         var moveFeedback = gsap.timeline();
 
         moveFeedback
-        .to('#feedbackDescription', {duration: 1, x: 0, ease: 'power2.out'})
+        .to('#feedbackDescription', {duration: 1, x: 0, ease: 'power2.out'});
+
+        if(viewFeedback == true){
+            var except = document.getElementById('feedbackDescription');
+
+            document.addEventListener('click', closeThisFeedback, true);
+            function closeThisFeedback(e){
+                if ( !except.contains(e.target) ) { //if the clicked element is the feedback div then it wont disappear
+                    var listID = document.getElementById('feedbackDescription');
+                    let viewFeedbackTimeline = gsap.timeline();
+                    viewFeedbackTimeline
+                    .to('#feedbackDescription', {duration: 0.5, x: 310, ease: 'power2.in'});
+                }
+            }
+            viewFeedback = false;
+        }
     }
 
     function hideFeedbackMessage(){
